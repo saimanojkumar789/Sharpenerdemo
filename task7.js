4a8711285d5690430af1ccaa3e0c345fdaeb6b20
872677 <saimanojkumar789@gmail.com>
2023-02-18 23:31:17 +0530

    final commit task 7

--- task7.js
+++ task7.js
@@ -10,10 +10,15 @@ console.log(items.nextElementSibling);
 console.log(items.previousSibling);
 console.log(items.previousElementSibling);
 var newDiv = document.createElement('div');
-var newDivText = document.createTextNode('Hello world');
+var newDivText = document.createTextNode('Hello ');
 newDiv.appendChild(newDivText);
 newDiv.setAttribute('title','Hello div');
 
 var container = document.querySelector('header .container');
 var h1 = document.querySelector('header h1');
 container.insertBefore(newDiv,h1);
+
+var greet = document.createElement('div');
+var greetText = document.createTextNode('Hello');
+greet.appendChild(greetText);
+items.insertBefore(greet,items.children[0]);
